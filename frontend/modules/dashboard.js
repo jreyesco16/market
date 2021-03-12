cd93b3eaf7f74ac01e71cf663ee215ddf7f08265
jreyesco16 <jesserc.2@gmail.com>
2021-03-12 14:55:02 -0600

    will not be implementing vue as of right now

--- frontend/modules/dashboard.js
+++ frontend/modules/dashboard.js
@@ -2,23 +2,9 @@ const { response } = require('express')
 const { readFile } = require('fs')
 require('cookie-parser')
 const { authenticate }= require('../component/authenticate');
-const Vue = require('vue');
-const { default: fetch } = require('node-fetch');
-const renderer = require('vue-server-renderer').createRenderer()
+
 
 function dashboard(req, res){
-
-
-    // VUE INITIALIZATION
-    const vue = new Vue({
-        data: {
-                url: req.url,
-                backend_url : '',
-                user_data: {}
-        },
-        // MUST add a template
-        template: require('fs').readFileSync('./html/dashboard.html','utf-8')
-    })
 
     if(req.cookies.market_token != undefined){
 
@@ -34,15 +20,6 @@ function dashboard(req, res){
                 }
                 res.send(html)
             });
-            // render the html file with vue components
-            // renderer.renderToString(vue, (err, html) => {
-            //     if (err) {
-            //         res.status(500).end("Internal Server Error")
-            //         return
-            //     }
-
-            //     res.end(html)
-            // })
 
         }else{
             res.redirect("/")
